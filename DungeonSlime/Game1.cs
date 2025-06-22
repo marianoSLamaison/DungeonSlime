639c1a27dc0757a7dc77d7987388ebb752e34436
m <m>
2025-06-22 00:35:24 -0300

    Finalizada leccion N°4/27

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace DungeonSlime;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+    //private GraphicsDeviceManager _graphics;
+    //private SpriteBatch _spriteBatch;
 
-    public Game1()
+    public Game1() : base("DungeonSlime", 1280, 720, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        //_graphics = new GraphicsDeviceManager(this);
+        //Content.RootDirectory = "Content";
+        //IsMouseVisible = true;
     }
 
     protected override void Initialize()
@@ -25,9 +26,10 @@ public class Game1 : Game
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        //_spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
---
+++ MonoGameLibrary/Core.cs
@@ -0,0 +1,97 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameLibrary;
+
+public class Core : Game
+{
+    //Nota, internal lo que hace segun una leida rapida, es que impide que el valor sea accedido por
+    //algo que no sea parte del mismo assembly, lo cual segun ejemplos de windows, produciria por ejemplo
+    //un crasheo durante la compilacion si un archivo intenta instanciar a otroa que tiene keywords internal
+    // y no pertenece a su mismo assembly
+    //en este caso, nada que no pertenesca al mismo ensamblado que Core podria acceder a s_instance
+    //lo cual impediria que cosas fuera de la libreria la puedan tocar.
+    internal static Core s_instance;
+
+    /// <summary>
+    /// Gets a reference to the Core instance.
+    /// </summary>
+    public static Core Instance => s_instance;
+
+    /// <summary>
+    /// Gets the graphics device manager to control the presentation of graphics.
+    /// </summary>
+    public static GraphicsDeviceManager Graphics { get; private set; }
+
+    /// <summary>
+    /// Gets the graphics device used to create graphical resources and perform primitive rendering.
+    /// </summary>
+    public static new GraphicsDevice GraphicsDevice { get; private set; }
+
+    /// <summary>
+    /// Gets the sprite batch used for all 2D rendering.
+    /// </summary>
+    public static SpriteBatch SpriteBatch { get; private set; }
+
+    /// <summary>
+    /// Gets the content manager used to load global assets.
+    /// </summary>
+    public static new ContentManager Content { get; private set; }
+
+    /// <summary>
+    /// Creates a new Core instance.
+    /// </summary>
+    /// <param name="title">The title to display in the title bar of the game window.</param>
+    /// <param name="width">The initial width, in pixels, of the game window.</param>
+    /// <param name="height">The initial height, in pixels, of the game window.</param>
+    /// <param name="fullScreen">Indicates if the game should start in fullscreen mode.</param>
+    public Core(string title, int width, int height, bool fullScreen)
+    {
+        // Ensure that multiple cores are not created.
+        if (s_instance != null)
+        {
+            throw new InvalidOperationException($"Only a single Core instance can be created");
+        }
+
+        // Store reference to engine for global member access.
+        s_instance = this;
+
+        // Create a new graphics device manager.
+        Graphics = new GraphicsDeviceManager(this);
+
+        // Set the graphics defaults
+        Graphics.PreferredBackBufferWidth = width;
+        Graphics.PreferredBackBufferHeight = height;
+        Graphics.IsFullScreen = fullScreen;
+
+        // Apply the graphic presentation changes
+        Graphics.ApplyChanges();
+
+        // Set the window title
+        Window.Title = title;
+
+        // Set the core's content manager to a reference of hte base Game's
+        // content manager.
+        Content = base.Content;
+
+        // Set the root directory for content
+        Content.RootDirectory = "Content";
+
+        // Mouse is visible by default
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        // Set the core's graphics device to a reference of the base Game's
+        // graphics device.
+        GraphicsDevice = base.GraphicsDevice;
+
+        // Create the sprite batch instance.
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+}
